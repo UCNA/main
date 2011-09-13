44f41a40f8a53fe024a0f4e5ea8deb295533684e
Kevin Peter Hickerson <kevinh@ucn.(none)>
2011-09-12 23:56:57 -0700

    Updated code from Michael.

--- ExtractFierzTerm.cc
+++ ExtractFierzTerm.cc
@@ -2,6 +2,7 @@
 #include "G4toPMT.hh"
 #include "CalDBSQL.hh"
 #include <TH1F.h>
+#include <TLegend.h>
 
 static double electron_mass = 510.9989; // needed for the physics pf Fierz interference
 
@@ -10,17 +11,22 @@ class FierzHistogram {
     double minBin;
     double maxBin;
     unsigned int nBins;
-    TH1F *fierz_histogram;
-    TH1F *sm_histogram;
-    //TH1F *ucna_data_histogram;
-    //TH1F *backgrund_histogram;
+    TH1F *fierz_super_sum_histogram;
+    TH1F *sm_super_sum_histogram;
+    TH1F* fierz_histogram[2][2];
+    TH1F* sm_histogram[2][2];
 
     FierzHistogram( double _minBin, double _maxBin, unsigned int _nBins) {
         minBin = _minBin;
         maxBin = _maxBin;
         nBins = _nBins;
-        fierz_histogram = new TH1F("fierz_histogram", "", nBins, minBin, maxBin);
-        sm_histogram = new TH1F("standard_model_histogram", "", nBins, minBin, maxBin);
+        fierz_super_sum_histogram = new TH1F("fierz_histogram", "", nBins, minBin, maxBin);
+        sm_super_sum_histogram = new TH1F("standard_model_histogram", "", nBins, minBin, maxBin);
+        for (int side = 0; side < 2; side++)
+            for (int spin = 0; spin < 2; spin++) {
+                fierz_histogram[side][spin] = new TH1F("fierz_super_sum", "", nBins, minBin, maxBin);
+                sm_histogram[side][spin] = new TH1F("standard_model_super_sum", "", nBins, minBin, maxBin);
+            }
     }
 
 /*
@@ -37,7 +43,7 @@ class FierzHistogram {
     }
 };
 
-FierzHistogram betaSpectrum(0,1000,400);
+FierzHistogram mc(0,1000,40);
 
 /**
  * x[0] : kenetic energy
@@ -45,13 +51,13 @@ FierzHistogram betaSpectrum(0,1000,400);
  */
 double theoretical_fierz_spectrum(double *x, double*p) {
     double rv = 0;
-    //unsigned n = betaSpectrum.sm_histogram->FindBin(p[3]*x[0]*x[0] + p[2]*x[0] - p[1]);        
-    unsigned n = betaSpectrum.sm_histogram->FindBin(p[2]*x[0] - p[1]);        
-    //unsigned n = betaSpectrum.sm_histogram->FindBin(x[0]);        
+    //unsigned n = mc.sm_histogram->FindBin(p[3]*x[0]*x[0] + p[2]*x[0] - p[1]);        
+    unsigned n = mc.sm_super_sum_histogram->FindBin(p[2]*x[0] - p[1]);        
+    //unsigned n = mc.sm_histogram->FindBin(x[0]);        
     double b = p[0];
     double norm = 1 + 0.65 * b;
-    rv += betaSpectrum.sm_histogram->GetBinContent(n) / norm;
-    rv += b * 0.65 * betaSpectrum.fierz_histogram->GetBinContent(n) / norm;
+    rv += mc.sm_super_sum_histogram->GetBinContent(n) / norm;
+    rv += b * 0.65 * mc.fierz_super_sum_histogram->GetBinContent(n) / norm;
     return rv;
 }
 
@@ -72,15 +78,35 @@ void normalize(TH1F* hist) {
 TH1F* compute_super_ratio(TH1F* rate_histogram[2][2] ) {
     TH1F *super_ratio_histogram = new TH1F(*(rate_histogram[0][0]));
     int bins = super_ratio_histogram->GetNbinsX();
-    for (int bin = 0; bin < bins; bin++) {
+    for (int bin = 1; bin < bins+2; bin++) {
         double r[2][2];
         for (int side = 0; side < 2; side++)
             for (int spin = 0; spin < 2; spin++)
                 r[side][spin] = rate_histogram[side][spin]->GetBinContent(bin);
         double super_ratio = (r[0][0] * r[1][1]) / (r[0][1] * r[1][0]);
         super_ratio_histogram->SetBinContent(bin, super_ratio);
+        super_ratio_histogram->SetBinError(bin, 0.01);
     }
     return super_ratio_histogram;
+}
+
+// S = (r[0][0] * r[1][1]) / (r[0][1] * r[1][0]);
+TH1F* compute_super_sum(TH1F* rate_histogram[2][2] ) {
+    TH1F *super_sum_histogram = new TH1F(*(rate_histogram[0][0]));
+    int bins = super_sum_histogram->GetNbinsX();
+    for (int bin = 1; bin < bins+2; bin++) {
+        double r[2][2];
+        for (int side = 0; side < 2; side++)
+            for (int spin = 0; spin < 2; spin++)
+                r[side][spin] = rate_histogram[side][spin]->GetBinContent(bin);
+        double super_sum = TMath::Sqrt(r[0][0] * r[1][1]) + TMath::Sqrt(r[0][1] * r[1][0]);
+        if ( TMath::IsNaN(super_sum) ) 
+            super_sum = 0;
+        super_sum_histogram->SetBinContent(bin, super_sum);
+        printf("Setting bin content for super sum bin %d, to %f\n", bin, super_sum);
+        super_sum_histogram->SetBinError(bin, 0.0);
+    }
+    return super_sum_histogram;
 }
 
 int main(int argc, char *argv[]) {
@@ -94,7 +120,7 @@ int main(int argc, char *argv[]) {
 	// PMT Calibrator loads run-specific energy calibrations info for selected run (14111)
 	// and uses default Calibrations DB connection to most up-to-date though possibly unstable "mpm_debug"
 	RunNum rn = 14111;
-	PMTCalibrator PCal(rn,CalDBSQL::CDB);
+	PMTCalibrator PCal(rn,CalDBSQL::getCDB());
 	
 	// Energy simulators for both sides using same PMT Calibrator
 	PMTGenerator PGenE;
@@ -104,7 +130,7 @@ int main(int argc, char *argv[]) {
 	// set the data scanner to use these PMT Calibrators
 	G2P.setGenerators(&PGenE,&PGenW);
 
-	unsigned int nToSim = 1E5;	// how many triggering events to simulate
+	unsigned int nToSim = 1E4;	// how many triggering events to simulate
 	unsigned int nSimmed = 0;	// counter for how many (triggering) events have been simulated
 	
     /*
@@ -114,8 +140,7 @@ int main(int argc, char *argv[]) {
     TH1F *fierz_histogram = new TH1F("fierz_histogram", "", nBins, minBin, maxBin);
     TH1F *sm_histogram = new TH1F("standard_model_histogram", "", nBins, minBin, maxBin);
     */
-    //FierzHistogram betaSpectrum(0,1000,40);
-
+    //FierzHistogram mc(0,1000,40);
 
 	// start a scan over the data. Argument "true" means start at random offset in file instead of at beginning
 	// if you really want this to be random, you will need to seed rand() with something other than default
@@ -139,18 +164,25 @@ int main(int argc, char *argv[]) {
 				continue;
 			
 			// print out event info, (simulated) reconstructed true energy and position, comparable to values in data
-			printf("Event on side %c: type=%i, Etrue=%g @ position (%g,%g)\n",
-				   sideNames(s), tp, G2P.getEtrue(), G2P.wires[s][X_DIRECTION].center, G2P.wires[s][Y_DIRECTION].center);
+			printf("Event on side %c: type=%i, Etrue=%g @ position (%g,%g), %d\n",
+				   sideNames(s), tp, G2P.getEtrue(), G2P.wires[s][X_DIRECTION].center, G2P.wires[s][Y_DIRECTION].center, (unsigned)G2P.afp);
 
 			// print out event primary info, only available in simulation
 			printf("\tprimary KE=%g, cos(theta)=%g\n",G2P.ePrim,G2P.costheta);
 
+            /*
             double energy = G2P.ePrim + electron_mass;
             double fierz_weight = electron_mass / energy;
             if (nSimmed % 2)
-                betaSpectrum.fierz_histogram->Fill(G2P.getEtrue(), fierz_weight);
+                mc.fierz_histogram->Fill(G2P.getEtrue(), fierz_weight);
             else
-                betaSpectrum.sm_histogram->Fill(G2P.getEtrue(), 1);
+                */
+            
+            //if (G2P.afp == AFP_ON)
+            if (nSimmed % 100 > 40) // redo with real loading eff.
+                mc.sm_histogram[s][0]->Fill(G2P.getEtrue(), 1);
+            else
+                mc.sm_histogram[s][1]->Fill(G2P.getEtrue(), 1);
 
 			nSimmed++;
 		}
@@ -159,16 +191,27 @@ int main(int argc, char *argv[]) {
 		if(nSimmed>=nToSim)
 			break;
 	}
-    betaSpectrum.normalizeHistogram(betaSpectrum.fierz_histogram);
-    betaSpectrum.normalizeHistogram(betaSpectrum.sm_histogram);
-    betaSpectrum.fierz_histogram->SetLineColor(3);
-    betaSpectrum.sm_histogram->SetLineColor(4);
-	
+    
+    mc.sm_super_sum_histogram = compute_super_sum(mc.sm_histogram);
+    normalize(mc.sm_super_sum_histogram);
+
+    mc.fierz_super_sum_histogram = compute_super_sum(mc.fierz_histogram);
+    normalize(mc.fierz_super_sum_histogram);
+
+    for (int side = 0; side < 2; side++)
+        for (int spin = 0; spin < 2; spin++) {
+            normalize(mc.fierz_histogram[side][spin]);
+            normalize(mc.sm_histogram[side][spin]);
+        }
+
     TCanvas *canvas = new TCanvas("fierz_canvas", "Fierz component of energy spectrum");
-    betaSpectrum.fierz_histogram->Draw("");
-    betaSpectrum.sm_histogram->Draw("Same");
+    mc.fierz_super_sum_histogram->SetLineColor(3);
+    mc.sm_super_sum_histogram->SetLineColor(4);
+    mc.fierz_super_sum_histogram->Draw("");
+    mc.sm_super_sum_histogram->Draw("Same");
 
     // fit a smooth model to the mc
+    /*
     TF1 *mc_fit = new TF1("fierz_mc_fit", mc_model, 0, 1000, deg+1);
     mc_fit->SetParameter(0,-0.5);
     mc_fit->SetParameter(1,1.0);
@@ -177,10 +220,11 @@ int main(int argc, char *argv[]) {
     mc_fit->SetParameter(4,1.0);
     //mc_fit->SetParameter(5,1.0);
     //mc_fit->SetParameter(6,1.0);
-    betaSpectrum.sm_histogram->Fit("fierz_mc_fit");
+    mc.sm_histogram->Fit("fierz_mc_fit");
 
     TString pdf_filename = "/data/kevinh/mc/fierz_models_to_fit.pdf";
     canvas->SaveAs(pdf_filename);
+    */
 
     /*
         If you want a fast way to get the combined data spectrums for comparison, I already have it extracted as a ROOT histogram for my own data/MC comparisons.
@@ -212,9 +256,10 @@ int main(int argc, char *argv[]) {
         // normalize after background subtraction
         background_histogram->Draw("");
     */
-    normalize(ucna_data_histogram[0][0]);
+    //normalize(ucna_data_histogram[0][0]);
     ucna_data_histogram[0][0]->Draw("Same");
 
+    /*
     TF1 *fit = new TF1("fierz_fit", theoretical_fierz_spectrum, 0, 1000, 3);
     fit->SetParameter(0,0.0);
     fit->SetParameter(1,0.0);
@@ -223,11 +268,32 @@ int main(int argc, char *argv[]) {
     double chisq = fit->GetChisquare();
     double N = fit->GetNDF();
     printf("Chi^2 / ( N - 1) = %f / %f = %f\n",chisq, N-1, chisq/(N-1));
+    */
 
     TString fit_pdf_filename = "/data/kevinh/mc/fierz_fit_data.pdf";
     canvas->SaveAs(fit_pdf_filename);
 
     TH1F *super_ratio_histogram = compute_super_ratio(ucna_data_histogram);
+    super_ratio_histogram->Draw();
+    TString super_ratio_pdf_filename = "/data/kevinh/mc/super_ratio_data.pdf";
+    canvas->SaveAs(super_ratio_pdf_filename);
+
+    TH1F *super_sum_histogram = compute_super_sum(ucna_data_histogram);
+    normalize(super_sum_histogram);
+    mc.sm_super_sum_histogram->Draw();
+    super_sum_histogram->Draw("same");
+
+    TLegend * legend = new TLegend(0.6,0.8,0.7,0.65);
+    legend->AddEntry(super_sum_histogram, "Super sum", "l");
+    legend->AddEntry(mc.sm_super_sum_histogram, "Monte Carlo", "l");
+    //legend->AddEntry(mc.sm_super_sum_histogram, "Bonehead sum", "l");
+    legend->SetTextSize(0.04);
+    legend->SetBorderSize(0);
+    legend->Draw();
+
+    TString super_sum_pdf_filename = "/data/kevinh/mc/super_sum_data.pdf";
+    canvas->SaveAs(super_sum_pdf_filename);
+
 
 	return 0;
 }
