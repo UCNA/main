66d0260a67e3fc9561eb5dd5290b4388d5e4bfa2
Michael Mendenhall <mpm@caltech.edu>
2012-06-15 16:43:39 -0400

    Energy uncertainty using fit to MC for asymmetry energy dependence

--- Scripts/Corrections/EnergyErrors.py
+++ Scripts/Corrections/EnergyErrors.py
@@ -2,6 +2,7 @@
 
 import sys
 sys.path.append("..")
+from math import *
 from PyxUtils import *
 from ErrorEnvelope import *
 
@@ -16,16 +17,32 @@ def writeUncertaintyTable(fout,dat):
 	for d in dat:
 		fout.write("%i\t%i\t%f\t%f\n"%d)
 
+def neutronBeta(KE, m = 511.):
+	return sqrt(KE**2+2*m*KE)/(m+KE)
+
+def ObsAsymApprox(KE):
+	"""Phenomenological fit to MC observed asymmetry"""
+	A0 = .1172
+	p2 = .966648
+	p3 = .0001174
+	p4 = 56.5529
+	p5 = 12.8861
+	p6 = 2.18673
+	return A0*neutronBeta(KE)*0.5*p2*(1+p3*KE)*(1+p6/(1+exp((KE-p4)/p5)))
+
 def linearityUncertaintyTable():
 	"""Uncertainty due to energy calibration errors; see EnergyErrorsRevis.pdf"""
 	edges = bin_edges()
 	dat = []
-	def neutronBeta(KE, m = 511.):
-		return sqrt(KE**2+2*m*KE)/(m+KE)
-	for i in range(len(edges)-1):
+	errmax = 0
+	for i in range(len(edges)-1)[-1::-1]:
 		c = 0.5*(edges[i]+edges[i+1])
 		Eprim = c+calEnvelope(c)
-		dat.append((edges[i],edges[i+1],0.0,neutronBeta(Eprim)/neutronBeta(c)-1))
+		err = ObsAsymApprox(Eprim)/ObsAsymApprox(c)-1
+		if err > errmax:
+			errmax = err
+		dat.append((edges[i],edges[i+1],0.0,errmax))
+	dat = dat[-1::-1]
 	fout = open(baseOutPath+"/EnergyLinearityUncertainty_2010.txt","w")
 	fout.write("# Uncertainty from energy calibration linearity envelope for 2010 data\n")
 	writeUncertaintyTable(fout,dat)
@@ -41,4 +58,4 @@ def gainFluctsUncertaintyTable():
 							
 if __name__=="__main__":
 	linearityUncertaintyTable()
-#gainFluctsUncertaintyTable()
+	#gainFluctsUncertaintyTable()
