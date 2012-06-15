14678593a8459fd297c645305241094e4d93edff
Michael Mendenhall <mpm@caltech.edu>
2012-06-15 12:00:13 -0400

    Generated energy linearity uncertainty table

--- Scripts/Corrections/EnergyErrors.py
+++ Scripts/Corrections/EnergyErrors.py
@@ -1,17 +1,18 @@
-#!/sw/bin/python2.6
+#!/usr/bin/python
 
 import sys
 sys.path.append("..")
 from PyxUtils import *
 from ErrorEnvelope import *
 
-def bin_edges(w=25,n=32):
+def bin_edges(w=10,n=100):
 	return [w*i for i in range(n+1)]
 
-def writeUncertaintyTable(fname,dat):
+baseOutPath = "../../Aux/Corrections/"
+
+def writeUncertaintyTable(fout,dat):
 	"""Write uncertainties table to file in correct format"""
-	fout = open(fname,"w")
-	fout.write("#bin_lo\tbin_up\tcorrection\tuncertainty\n")
+	fout.write("#E_lo\tE_hi\tcorrection\tuncertainty\n")
 	for d in dat:
 		fout.write("%i\t%i\t%f\t%f\n"%d)
 
@@ -25,7 +26,9 @@ def linearityUncertaintyTable():
 		c = 0.5*(edges[i]+edges[i+1])
 		Eprim = c+calEnvelope(c)
 		dat.append((edges[i],edges[i+1],0.0,neutronBeta(Eprim)/neutronBeta(c)-1))
-	writeUncertaintyTable("linearityUncertainty.txt",dat)
+	fout = open(baseOutPath+"/EnergyLinearityUncertainty_2010.txt","w")
+	fout.write("# Uncertainty from energy calibration linearity envelope for 2010 data\n")
+	writeUncertaintyTable(fout,dat)
 	
 def gainFluctsUncertaintyTable():
 	"""Uncertainty due to gain fluctuations; see"""
@@ -38,4 +41,4 @@ def gainFluctsUncertaintyTable():
 							
 if __name__=="__main__":
 	linearityUncertaintyTable()
-	gainFluctsUncertaintyTable()
+#gainFluctsUncertaintyTable()
