7406e82ad3698aaa1eb728f5142b2a320cfbd8de
surfseatown <rpattie1115@gmail.com>
2013-08-29 14:34:47 -0400

    Added in call for different East/West decay trap thicknesses

--- ucnG4_dev/include/bmDecayTrapConstruction.hh
+++ ucnG4_dev/include/bmDecayTrapConstruction.hh
@@ -9,23 +9,30 @@
 class bmDecayTrapConstruction: public MaterialUser {
 public:
 	/// constructor
-	bmDecayTrapConstruction(): fWindowThick(0.5*um), fCoatingThick(0.15*um), fIRtrap(2.45*inch), decayTube_Wall(2*mm),
+	bmDecayTrapConstruction(): fIRtrap(2.45*inch), decayTube_Wall(2*mm),
 	fIRcollimator(2.3*inch), fTubeMat(Cu), fCollimatorMat(Polyethylene), fWindowMat(Mylar), fCoatingMat(Be) { }
 	
-	G4double fWindowThick;		//< window thickness
-	G4double fCoatingThick;		//< window Be coating thickness
-	G4double fIRtrap;			//< decay trap IR
+	G4double fWindowThick[2];
+	G4double fCoatingThick[2];
+	G4double thicknessOfTrapWindow[2];
+        G4double beWinPosZ[2];
+        G4double mylarWinPosZ[2];
+        G4double trap_winPosZ[2];
+        G4double fIRtrap;		//< decay trap IR
 	G4double decayTube_Wall;	//< decay trap wall thickness
 	G4double fIRcollimator;		//< collimator IR
 	G4Material* fTubeMat;		//< decay tube material
 	G4Material* fCollimatorMat;	//< collimator material
 	G4Material* fWindowMat;		//< decay tube window material
 	G4Material* fCoatingMat;	//< decay tube coating material
+	G4Tubs* trap_win_tube[2];
+        G4Tubs* mylarTube[2];        
+        G4Tubs* beTube[2];
 
 	G4LogicalVolume* decayTube_log;			//< decay trap tube
 	G4LogicalVolume* trap_win_log[2];		//< trap window volume
 	G4LogicalVolume* mylar_win_log[2];		//< mylar layer of window
-	G4LogicalVolume* be_win_log[2];			//< berillium layer of window
+	G4LogicalVolume* be_win_log[2];			//< beryllium layer of window
 	G4LogicalVolume* trap_monitor_log[2];	//< extra event monitoring region
 	G4LogicalVolume* collimator_log[2];		//< collimator
 	G4LogicalVolume* collimatorBack_log[2];	//< bracket behind collimator
