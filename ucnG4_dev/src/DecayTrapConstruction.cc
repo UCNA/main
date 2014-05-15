1deed0e728e6b17b5bd92f5ed31a6730907596c7
Michael Brown <michael.brown1@uky.edu>
2014-05-15 15:44:54 -0400

    Fixed DecayTrapConstruction.cc and got simulation code to compile

--- ucnG4_dev/src/DecayTrapConstruction.cc
+++ ucnG4_dev/src/DecayTrapConstruction.cc
@@ -4,7 +4,8 @@
 #include <G4Tubs.hh>
 #include <G4RotationMatrix.hh>
 
-void DecayTrapConstruction::Construct(G4LogicalVolume* world, double crinkleAngle) {
+void DecayTrapConstruction::Construct(G4LogicalVolume* world, double crinkleAngle) 
+{
 	
 	////////////////////////////////////////
 	// decay tube
@@ -31,20 +32,11 @@ void DecayTrapConstruction::Construct(G4LogicalVolume* world, double crinkleAngl
 	// Trap windows, collimator, monitors
 	////////////////////////////////////////
         const G4double decayTubeL[2] = {3.0*m,3.0*m};
-        for(Side s = EAST; s <= WEST; ++s) {
-
-        thicknessOfTrapWindow[s] = fWindowThick[s]+fCoatingThick[s];
-	const G4double collimator_thick = fColThickness; 	
-        beWinPosZ[s] = fCoatingThick[s]/2.-thicknessOfTrapWindow[s]/2.;
-	mylarWinPosZ[s] = thicknessOfTrapWindow[s]/2.-fWindowThick[s]/2.;
-	trap_winPosZ[s] = (decayTubeL[s]+thicknessOfTrapWindow[s])/2.;
+	const G4double collimator_thick = fColThickness; 
 	const G4double trap_monitor_thickness = 1.0*mm;
 	const G4double trap_monitor_posZ = 0.5*m;
-	
-        trap_win_tube[s] = new G4Tubs("trap_win_tube",0.,decayTube_OR,thicknessOfTrapWindow[s]/2.,0.,2*M_PI);
-        mylarTube[s] = new G4Tubs("mylarTube",0.,decayTube_OR,fWindowThick[s]/2.,0.,2*M_PI);
-        beTube[s] = new G4Tubs("beTube",0.,decayTube_OR,fCoatingThick[s]/2.,0.,2*M_PI);
-        G4Tubs* collimatorTube = new G4Tubs("collimatorTube",fIRcollimator,fIRcollimator+collimator_thick,fColLength/2.,0.,2*M_PI);
+
+	G4Tubs* collimatorTube = new G4Tubs("collimatorTube",fIRcollimator,fIRcollimator+collimator_thick,fColLength/2.,0.,2*M_PI);
         G4Tubs* collimatorBackTube = new G4Tubs("collimatorBackTube",decayTube_OR+1.*mm,fIRcollimator+collimator_thick,fColLength,0.,2*M_PI);
         G4Tubs* trap_monitor_tube = new G4Tubs("trap_monitor_tube",0.,fIRtrap,trap_monitor_thickness/2,0.,2*M_PI);	
 	G4VisAttributes* visWindow = new G4VisAttributes(G4Colour(0,1.0,0,1));
@@ -52,18 +44,27 @@ void DecayTrapConstruction::Construct(G4LogicalVolume* world, double crinkleAngl
 	G4RotationMatrix* wigRot = new G4RotationMatrix;  
 	wigRot->rotateY(M_PI/2.*rad);
 	wigRot->rotateX(M_PI/2.*rad);
-	
+		
 	for(Side sd = EAST; sd <= WEST; ++sd) {
+
+	        thicknessOfTrapWindow[sd] = fWindowThick[sd]+fCoatingThick[sd];		
+                beWinPosZ[sd] = fCoatingThick[sd]/2.-thicknessOfTrapWindow[sd]/2.;
+	        mylarWinPosZ[sd] = thicknessOfTrapWindow[sd]/2.-fWindowThick[sd]/2.;
+	        trap_winPosZ[sd] = (decayTubeL[sd]+thicknessOfTrapWindow[sd])/2.;
+	
+                trap_win_tube[sd] = new G4Tubs("trap_win_tube",0.,decayTube_OR,thicknessOfTrapWindow[sd]/2.,0.,2*M_PI);
+                mylarTube[sd] = new G4Tubs("mylarTube",0.,decayTube_OR,fWindowThick[sd]/2.,0.,2*M_PI);
+                beTube[sd] = new G4Tubs("beTube",0.,decayTube_OR,fCoatingThick[sd]/2.,0.,2*M_PI);
 		
 		if(crinkleAngle) wigglefoils[sd].thetaMax = crinkleAngle;
 		wigglefoils[sd].containerMat = Vacuum;
 		wigglefoils[sd].length = 2*decayTube_OR;
 		wigglefoils[sd].nseg = int(4*decayTube_OR/wigglefoils[sd].period)+1;
-		wigglefoils[sd].addLayer(fWindowMat,fWindowThick,true,visWindow);
-		wigglefoils[sd].addLayer(fCoatingMat,fCoatingThick,true,visWindow);
+		wigglefoils[sd].addLayer(fWindowMat,fWindowThick[sd],true,visWindow);
+		wigglefoils[sd].addLayer(fCoatingMat,fCoatingThick[sd],true,visWindow);
 		wigglefoils[sd].Construct();
 		
-		trap_win_log[sd] = new G4LogicalVolume(trap_win_tube,Vacuum,sideSubst("trap_win_log%c",sd));
+		trap_win_log[sd] = new G4LogicalVolume(trap_win_tube[sd],Vacuum,sideSubst("trap_win_log%c",sd));
 		trap_win_log[sd]->SetVisAttributes(visWindow);
 		
 		if(crinkleAngle) {
@@ -93,20 +94,20 @@ void DecayTrapConstruction::Construct(G4LogicalVolume* world, double crinkleAngl
 		new G4PVPlacement(NULL,G4ThreeVector(0.,0.,ssign(sd)*collimatorBackZ),collimatorBack_log[sd],
 						  sideSubst("collimatorBack%c",sd),world,false,0);*/
 		  //=======
-			new G4PVPlacement(NULL,G4ThreeVector(0.,0.,ssign(sd)*trap_winPosZ),trap_win_log[sd],
+			new G4PVPlacement(NULL,G4ThreeVector(0.,0.,ssign(sd)*trap_winPosZ[sd]),trap_win_log[sd],
 							  sideSubst("trap_win%c",sd),world,false,0);
 		}
 		
-		mylar_win_log[sd] = new G4LogicalVolume(mylarTube, fWindowMat, sideSubst("mylar_win_log%c",sd));
-		new G4PVPlacement(NULL,G4ThreeVector(0.,0.,ssign(sd)*mylarWinPosZ),mylar_win_log[sd],
+		mylar_win_log[sd] = new G4LogicalVolume(mylarTube[sd], fWindowMat, sideSubst("mylar_win_log%c",sd));
+		new G4PVPlacement(NULL,G4ThreeVector(0.,0.,ssign(sd)*mylarWinPosZ[sd]),mylar_win_log[sd],
 						  sideSubst("mylar_win%c",sd),trap_win_log[sd],false,0);
 		
-		be_win_log[sd] = new G4LogicalVolume(beTube, fCoatingMat,sideSubst("be_win_log%c",sd));
-		new G4PVPlacement(NULL,G4ThreeVector(0.,0.,ssign(sd)*beWinPosZ),be_win_log[sd],
+		be_win_log[sd] = new G4LogicalVolume(beTube[sd], fCoatingMat,sideSubst("be_win_log%c",sd));
+		new G4PVPlacement(NULL,G4ThreeVector(0.,0.,ssign(sd)*beWinPosZ[sd]),be_win_log[sd],
 						  sideSubst("be_win%c",sd),trap_win_log[sd],false,0);
 		
 		G4double collimatorPosZ = (decayTube_Length+fColLength)/2.;
-		collimatorPosZ += (crinkleAngle?wigglefoils[sd].getContainerThick():thicknessOfTrapWindow)/2.;
+		collimatorPosZ += (crinkleAngle?wigglefoils[sd].getContainerThick():thicknessOfTrapWindow[sd])/2.;
 		collimator_log[sd] = new G4LogicalVolume(collimatorTube, fCollimatorMat, sideSubst("collimator_log%c",sd));
 		G4double collimatorBackZ = decayTube_Length/2.-fColLength;
 		collimatorBack_log[sd] = new G4LogicalVolume(collimatorBackTube, fCollimatorMat, sideSubst("collimatorBack_log%c",sd));
@@ -120,4 +121,5 @@ void DecayTrapConstruction::Construct(G4LogicalVolume* world, double crinkleAngl
 		new G4PVPlacement(NULL,G4ThreeVector(0.,0.,ssign(sd)*trap_monitor_posZ),
 						  trap_monitor_log[sd],sideSubst("trap_monitor%c",sd),world,false,0);
 	}
+	
 }
