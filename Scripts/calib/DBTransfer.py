f20e6c38d4c27b5bec4fc72903f931bb0715c80c
Michael Mendenhall <mpm@caltech.edu>
2014-05-29 12:45:45 -0700

    Large increase in Calibration DB transfer speed

--- Scripts/calib/DBTransfer.py
+++ Scripts/calib/DBTransfer.py
@@ -5,31 +5,37 @@ sys.path.append("..")
 from ucnacore.EncalDB import *
 from string import join
 
+
+def dbInputStr(i):
+	if i is None or i=='':
+		return "Null"
+	return "'%s'"%str(i)
+
 class DBTransferManager:
+	"""Class for transferring calibration information from one version of DB to another"""
 	
 	def __init__(self, connIn, connOut):
 		self.connIn = connIn
 		self.connOut = connOut
 		self.tblcols = {}
 
-	def transfer_lines(self, tbl, qry, xargs = {}, fields = None, subgraphs = [], withPrimary = False):
+	def transfer_lines(self, tbl, qry, xargs = {}, subgraphs = [], withPrimary = False):
 		
-		# auto-generate field names
-		if fields is None:
-			if tbl not in self.tblcols:
-				cmd = "SHOW COLUMNS FROM `%s`"%tbl
-				if not withPrimary:
-					cmd += " WHERE `Key`!='PRI'"
-				self.connIn.execute(cmd)
-				s1 = frozenset([f[0] for f in self.connIn.fetchall()])
-				if self.connOut:
-					self.connOut.execute(cmd)
-					s2 = frozenset([f[0] for f in self.connOut.fetchall()])
-				else:
-					s2 = s1
-				self.tblcols[tbl] = s1.intersection(s2)
-				print "Table",tbl,"has columns",self.tblcols[tbl]
-			fields = [f for f in self.tblcols[tbl] if f not in xargs]
+		# identify table field names
+		if tbl not in self.tblcols:
+			cmd = "SHOW COLUMNS FROM `%s`"%tbl
+			if not withPrimary:
+				cmd += " WHERE `Key`!='PRI'"
+			self.connIn.execute(cmd)
+			s1 = frozenset([f[0] for f in self.connIn.fetchall()])
+			if self.connOut:
+				self.connOut.execute(cmd)
+				s2 = frozenset([f[0] for f in self.connOut.fetchall()])
+			else:
+				s2 = s1
+			self.tblcols[tbl] = s1.intersection(s2)
+			print "Table",tbl,"has columns",self.tblcols[tbl]
+		fields = [f for f in self.tblcols[tbl] if f not in xargs]
 
 		# select items to transfer
 		datlist = []
@@ -44,8 +50,11 @@ class DBTransferManager:
 			self.connIn.execute(cmd);
 			datlist = [[] for i in range(self.connIn.fetchone()[0])]
 		
+		if not datlist:
+			return None
+		
 		# copy over to other DB
-		inserts = []
+		cmd = None
 		for z in datlist:
 		
 			fdict = dict([(f,z[n]) for (n,f) in enumerate(fields)])
@@ -56,22 +65,23 @@ class DBTransferManager:
 			
 			fdict.update(xargs)
 			
-			cmd = "INSERT INTO `" + tbl + "` (" + join(["`%s`"%str(i) for i in fdict.keys()],',') + ") VALUES (" + join(["'%s'"%str(i) for i in fdict.values()],',') + ")"
-			print cmd
-			if self.connOut is not None:
-				self.connOut.execute(cmd)
-				self.connOut.execute("SELECT LAST_INSERT_ID()")
-				inserts.append( int(self.connOut.fetchone()[0]) )
+			if not cmd:
+				cmd = "INSERT INTO `" + tbl + "` (" + join(["`%s`"%i for i in fdict.keys()],',') + ") VALUES"
+			cmd += "\n\t(" + join([dbInputStr(i) for i in fdict.values()],',') + "),"
 		
-		if self.connOut is None:
-			return [-1,]
-		return inserts
+		cmd = cmd[:-1]
+		print cmd
+		if self.connOut is not None:
+			self.connOut.execute(cmd)
+			self.connOut.execute("SELECT LAST_INSERT_ID()")
+			return self.connOut.fetchone()[0]
 
+		return None
 
 	def transfer_graph(self,gid):
 		print
 		print "Relocating old graph",gid
-		iid = self.transfer_lines("graphs","graph_id = %i"%gid)[0]
+		iid = self.transfer_lines("graphs","graph_id = %i"%gid)
 		self.transfer_lines("graph_points","graph_id = %i"%gid, {"graph_id":iid})
 		return iid
 
@@ -80,31 +90,29 @@ class DBTransferManager:
 		if not iid:
 			print "No such position map!"
 			return None
-		else:
-			iid = iid[0]
 		self.transfer_lines("posmap_points", "posmap_set_id = %i"%pmid, {"posmap_set_id":iid})
 		return iid
 
 	def transfer_energy_calibration(self,ecid,pmid):
-		iid = self.transfer_lines("energy_calibration", "ecal_id = %i"%ecid, {"posmap_set_id":pmid})[0]
+		iid = self.transfer_lines("energy_calibration", "ecal_id = %i"%ecid, {"posmap_set_id":pmid})
 		self.transfer_lines("tube_calibration", "ecal_id = %i"%ecid, {"ecal_id":iid}, subgraphs=["linearity_graph"])
 
 	def transfer_mwpc_ecal(self,ecid,pmid):
-		return self.transfer_lines("mwpc_ecal", "mwpc_ecal_id = %i"%ecid, {"gain_posmap_id":pmid})[0]
+		return self.transfer_lines("mwpc_ecal", "mwpc_ecal_id = %i"%ecid, {"gain_posmap_id":pmid})
 
 	def transfer_evis_conversion(self,evcid):
-		return self.transfer_lines("evis_conversion", "evis_conversion_id = %i"%evcid, subgraphs=["conversion_curve_id"])[0]
+		return self.transfer_lines("evis_conversion", "evis_conversion_id = %i"%evcid, subgraphs=["conversion_curve_id"])
 
 	def transfer_cathscale(self,csid):
-		return self.transfer_lines("cath_ccloud_scale","cath_ccloud_scale_id = %i"%csid, subgraphs=["gain_graph_id"])[0]
+		return self.transfer_lines("cath_ccloud_scale", "cath_ccloud_scale_id = %i"%csid, subgraphs=["gain_graph_id"])
 
 	def transfer_cathcal_set(self, csid, transferShape=False):
-		ncsid = self.transfer_lines("cathcal_set", "cathcal_set_id = %i"%csid)[0]
+		ncsid = self.transfer_lines("cathcal_set", "cathcal_set_id = %i"%csid)
 		self.connIn.execute("SELECT cathseg_id FROM cathseg_cal WHERE cathcal_set_id=%i"%csid)
 		ocsgids = [l[0] for l in self.connIn.fetchall()]
 		print "Transferring cathseg calibrations",ocsgids
 		for ocsgid in ocsgids:
-			ncsgid = self.transfer_lines("cathseg_cal", "cathseg_id = %i"%ocsgid, {"cathcal_set_id":ncsid})[0]
+			ncsgid = self.transfer_lines("cathseg_cal", "cathseg_id = %i"%ocsgid, {"cathcal_set_id":ncsid})
 			if transferShape:
 				self.transfer_lines("cathshape_graphs", "cathseg_id = %i"%ocsgid, {"cathseg_id":ncsgid}, subgraphs=["graph_id"], withPrimary=True)
 		return ncsid
@@ -134,7 +142,7 @@ if __name__ == "__main__":
 	
 	#delete_all_entries(connOut); exit(0)
 	
-	if 0:
+	if 1:
 		pmid = dbtm.transfer_posmap(213)
 		dbtm.transfer_energy_calibration(9726,pmid)
 		if connOut is not None:
