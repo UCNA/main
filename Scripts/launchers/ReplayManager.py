1dc45f4a681ad2f1f1d856284a43eb8bdcdfe2f4
Michael Brown <michael.brown1@uky.edu>
2014-11-06 11:13:46 -0500

    Small edit to ReplayManager.py

--- Scripts/launchers/ReplayManager.py
+++ Scripts/launchers/ReplayManager.py
@@ -70,7 +70,7 @@ def processXeSim(rmin,rmax,nr):
 			nproc = 3
 		os.system("nice -n 15 parallel -P %i < xenon_simlist.txt"%nproc)
 		os.system("rm xenon_simlist.txt")
-		os.system(pcmd%(rmin,rmax,0,nr))
+		#os.system(pcmd%(rmin,rmax,0,nr))
 
 def betaOctetPositions(dosim):
 	pcmd = "cd "+anaBinDir+"; ./BetaOctetPositions %i\n"
