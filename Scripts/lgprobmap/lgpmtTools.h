9d0a1d6f1049eb7bd00d422887e0e66b1a8519d9
Chris Swank <cmswank@ubuntu.(none)>
2015-04-10 10:06:30 -0700

    adding stuff that hasent pushed  yet

--- Scripts/lgprobmap/lgpmtTools.h
+++ Scripts/lgprobmap/lgpmtTools.h
@@ -3,6 +3,5 @@
 
 double lightguideprob(double xpos, double ypos,int lgnum);
 
-double PMTprob(double xpos,double ypos, double offsetx,double offsety, double lga, double lgb, double lgc,int pmtnum);
 
 #endif
