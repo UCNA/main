e80356487e27d0c70aa6308822f10d3fd0eafc62
Kevin Peter Hickerson <hickerson@gmail.com>
2016-05-10 06:41:57 -0700

    Recovered deleted file.

--- Fierz/FierzFitter.cpp
+++ Fierz/FierzFitter.cpp
@@ -679,7 +679,8 @@ double UCNAmodel::compute_super_sum(double n[2][2], double e[2][2], double& S, d
                 }
                 else
                     varS += alter;             /// using Poisson stats
-            }
+            } else 
+                cout<<"Warning: Negative number in super sum error.\n";
         }
     }
 
@@ -783,7 +784,8 @@ double UCNAmodel::compute_asymmetry(int bin, double& A, double& sigmaA) {
 
     if (bin % 10 == 0) {
         double KE = super_sum.GetBinCenter(bin);
-        cout<<"Status: Asymmetry bin "<<bin<<" and KE "<<KE<<" set to "<<A<<"("<<sigmaA<<").\n";
+        cout<<"Status: Asymmetry bin "<<bin<<" and KE "
+            <<KE<<" set to "<<A<<"("<<sigmaA<<").\n";
     }
 
     return A;
@@ -923,20 +925,15 @@ double GetEntries(TH1D* histogram, double min, double max)
 	return N;
 }
 
-/*
-void normalize(TH1D* hist, double min, double max) 
-{
-	int bin_min = hist->FindBin(min);
-	int bin_max = hist->FindBin(max);
-	hist->Scale(1/(hist->GetBinWidth(2)*hist->Integral(_min, _max)));
-}
-*/
-
 
 double evaluate_expected_fierz(double min, double max, double integral_size = 1234) 
 {
-    TH1D *h1 = new TH1D("beta_spectrum_fierz", "Beta spectrum with Fierz term", integral_size, min, max);
-    TH1D *h2 = new TH1D("beta_spectrum", "Beta Spectrum", integral_size, min, max);
+    TH1D *h1 = new TH1D("beta_spectrum_fierz", 
+                        "Beta spectrum with Fierz term", 
+                        integral_size, min, max);
+    TH1D *h2 = new TH1D("beta_spectrum",
+                        "Beta Spectrum", 
+                        integral_size, min, max);
 	for (int i = 0; i < integral_size; i++)
 	{
 		double K = min + double(i)*(max-min)/integral_size;
@@ -949,6 +946,7 @@ double evaluate_expected_fierz(double min, double max, double integral_size = 12
 	}
 	return h1->Integral(0, integral_size) / h2->Integral(0, integral_size);
 }
+
 
 /// beta spectrum with little b term
 double fierz_beta_spectrum(const double *val, const double *par)
@@ -995,8 +993,12 @@ double beta_spectrum(const double *val, const double *par)
 double evaluate_expected_fierz(double m, double n, double min, double max, double integral_size = 1234)
 {
     // TODO These don't need to be pointers.
-    TH1D *h1 = new TH1D("beta_spectrum_fierz", "Beta spectrum with Fierz term", integral_size, min, max);
-    TH1D *h2 = new TH1D("beta_spectrum", "Beta Spectrum", integral_size, min, max);
+    TH1D *h1 = new TH1D("beta_spectrum_fierz", 
+                        "Beta spectrum with Fierz term", 
+                        integral_size, min, max);
+    TH1D *h2 = new TH1D("beta_spectrum", 
+                        "Beta Spectrum", 
+                        integral_size, min, max);
 	for (int i = 0; i < integral_size; i++)
 	{
 		double K = min + double(i)*(max-min)/integral_size;
@@ -1018,142 +1020,3 @@ double evaluate_expected_fierz(double min, double max)
 {
 	return evaluate_expected_fierz(0, 1, min, max, 1234);
 }
-
-
-
-#if 0
-
-/**
- * Si := (n[0][0] n[1][1]) / (n[0][1] * n[1][0])
- */
-TH1D* compute_super_ratio(TH1D* counts[2][2] ) 
-{
-    TH1D *super_ratio_histogram = new TH1D(*(counts[0][0]));
-    int bins = super_ratio_histogram->GetNbinsX();
-	std::cout << "Number of bins " << bins << std::endl;
-    for (int bin = 1; bin < bins+2; bin++) {
-        double n[2][2];
-        for (int side = 0; side < 2; side++)
-            for (int spin = 0; spin < 2; spin++)
-                n[side][spin] = counts[side][spin]->GetBinContent(bin);
-        double super_ratio = n[0][0]*n[1][1]/n[0][1]/n[1][0];
-        super_ratio_histogram->SetBinContent(bin, super_ratio);
-        super_ratio_histogram->SetBinError(bin, 0.01);   // TODO compute correctly!!
-    }
-    return super_ratio_histogram;
-}
-
-
-/**
- * S := n[0][0] + n[1][1] + n[0][1] + n[1][0]
- */
-TH1D* compute_super_sum(TH1D* histogram[2][2], TH1D* super_sum) 
-{
-    //TH1D *super_sum = new TH1D(*(counts[0][0]));
-    for (int side=0; side<2; side++)
-        for (int spin=0; spin<2; spin++)
-            if (not histogram[side][spin]) {
-                std::cout << "Error: histogram is not constructed.\n";
-                std::cout << "Side: "<< side << " Spin: " << spin <<".\n";
-                exit(1);
-            }
-
-    if (not super_sum) {
-        std::cout << "Error: super sum histogram is not constructed.\n";
-        exit(1);
-    }
-
-    int bins = super_sum->GetNbinsX();
-    for (int side = 0; side < 2; side++)
-        for (int spin = 0; spin < 2; spin++) {
-            int ss_bins = histogram[side][spin]->GetNbinsX();
-            if (bins != ss_bins and ss_bins == 0) {
-                std::cout << "Error: super sum and side spin histogram sizes don't match.\n";
-                std::cout << "super sum bins: " << bins << "\n";
-                exit(1);
-            }
-        }
-
-    for (int bin = 1; bin < bins+2; bin++) {
-        double n[2][2];
-        for (int side = 0; side < 2; side++)
-            for (int spin = 0; spin < 2; spin++)
-                n[side][spin] = histogram[side][spin]->GetBinContent(bin);
-
-        double super_sum = TMath::Sqrt(n[0][0] * n[1][1]) + TMath::Sqrt(n[0][1] * n[1][0]);
-        double rel_error = TMath::Sqrt( 1/n[0][0] + 1/n[1][0] + 1/n[1][1] + 1/n[0][1]);
-        if ( TMath::IsNaN(super_sum)) {
-            super_sum = 0;
-            std::cout << "Warning: super sum: division by zero.\n";
-        }
-
-        if (TMath::IsNaN(rel_error)) {
-			rel_error = 0;
-            std::cout << "Warning: super sum error: division by zero.\n";
-        }
-        
-        if (bin % 10 == 0)
-            printf("Setting bin content for super sum bin %d, to %f\n", bin, super_sum);
-
-        super_sum->SetBinContent(bin, super_sum);
-        super_sum->SetBinError(bin, super_sum*rel_error);
-    }
-    return super_sum;
-}
-
-
-TH1D& compute_asymmetry(TH1D* counts[2][2]) 
-{
-    TH1D&*asymmetry = new TH1D(*(counts[0][0]));
-    int bins = asymmetry->GetNbinsX();
-    for (int bin = 1; bin < bins+2; bin++) 
-	{
-        double n[2][2];
-        for (int side = 0; side < 2; side++)
-            for (int spin = 0; spin < 2; spin++)
-                n[side][spin] = counts[side][spin]->GetBinContent(bin);
-        double sqrt_super_ratio = TMath::Sqrt((n[0][0] * n[1][1]) / (n[0][1] * n[1][0]));
-        if ( TMath::IsNaN(sqrt_super_ratio) ) 
-            sqrt_super_ratio = 0;
-		double denom = 1 + sqrt_super_ratio;
-        double asymmetry = (1 - sqrt_super_ratio) / denom;
-		double sqrt_inverse_sum = TMath::Sqrt(1/n[0][0] + 1/n[1][1] + 1/n[0][1] + 1/n[1][0]);
-		double asymmetry_error = sqrt_inverse_sum * sqrt_super_ratio / (denom * denom);  
-        asymmetry->SetBinContent(bin, asymmetry);
-        asymmetry->SetBinError(bin, asymmetry_error);
-        //printf("Setting bin content for asymmetry bin %d, to %f\n", bin, asymmetry);
-    }
-    return asymmetry;
-}
-
-
-TH1D& compute_corrected_asymmetry(TH1D* counts[2][2], TH1D& correction) 
-{
-    TH1D&*asymmetry = new TH1D(*(counts[0][0]));
-    int bins = asymmetry->GetNbinsX();
-    for (int bin = 1; bin < bins+2; bin++) 
-	{
-        double n[2][2];
-        for (int side = 0; side < 2; side++)
-            for (int spin = 0; spin < 2; spin++)
-                n[side][spin] = counts[side][spin]->GetBinContent(bin);
-        double sqrt_super_ratio = TMath::Sqrt((n[0][0] * n[1][1]) / (n[0][1] * n[1][0]));
-        if ( TMath::IsNaN(sqrt_super_ratio) ) 
-            sqrt_super_ratio = 0;
-		double denom = 1 + sqrt_super_ratio;
-        double asymmetry = (1 - sqrt_super_ratio) / denom;
-		double sqrt_inverse_sum = TMath::Sqrt(1/n[0][0] + 1/n[1][1] + 1/n[0][1] + 1/n[1][0]);
-		double asymmetry_error = sqrt_inverse_sum * sqrt_super_ratio / (denom * denom);  
-		double K = asymmetry->GetBinCenter(bin);
-		double E = K + m_e;                   /// electron energy
-		double p = sqrt(E*E - m_e*m_e);       /// electron momentum
-		double beta = p / E;				  /// v/c
-        asymmetry->SetBinContent(bin, -2*asymmetry/beta);
-        asymmetry->SetBinError(bin, 2*asymmetry_error/beta);
-        asymmetry->Multiply(correction);
-        //printf("Setting bin content for corrected asymmetry bin %d, to %f\n", bin, asymmetry);
-    }
-    return asymmetry;
-}
-
-#endif
