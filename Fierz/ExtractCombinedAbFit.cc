e47d33b3fdd445095b0ace0376bda621cde45f8b
Kevin Peter Hickerson <hickerson@gmail.com>
2016-11-04 10:21:45 -0700

    Had to revert to a previous file because accidentally deleted before committing.

--- Fierz/ExtractCombinedAbFit.cc
+++ Fierz/ExtractCombinedAbFit.cc
@@ -106,7 +106,7 @@ void combined_chi2(Int_t & n, Double_t * /*grad*/ , Double_t &chi2, Double_t *p,
     assert(global_ff);
     if (FIT_TYPE=="AbN") {
         double A=p[0], b=p[1], N=p[2];
-	    chi2 = global_ff->combined_chi2(A,b,N);
+        chi2 = global_ff->combined_chi2(A,b,N);
     }
     else if (FIT_TYPE=="bN") {
         double b=p[0], N=p[1];
@@ -122,21 +122,21 @@ void combined_chi2(Int_t & n, Double_t * /*grad*/ , Double_t &chi2, Double_t *p,
 int cl = 14;
 void output_matrix(TString title, TMatrixD matrix, TString colNames[], TString rowNames[])
 {
-	cout<<"\n "<<title;
-	int cols = matrix.GetNcols();
-	int rows = matrix.GetNrows();
+    cout<<"\n "<<title;
+    int cols = matrix.GetNcols();
+    int rows = matrix.GetNrows();
     if (cols>1) {
         cout<<"\n     ";
         for (int i=0; i<cols; i++)
             cout<<setw(cl)<<colNames[i];
     }
-	cout<<"\n";
-	for (int i=0; i<cols; i++) {
+    cout<<"\n";
+    for (int i=0; i<cols; i++) {
         cout<<"    "<<rowNames[i];
-		for (int j=0; j<rows; j++)
-			cout<<setw(cl)<<matrix[i][j];
-	    cout<<"\n";
-	}
+        for (int j=0; j<rows; j++)
+            cout<<setw(cl)<<matrix[i][j];
+        cout<<"\n";
+    }
 }
 
 void output_matrix(TString title, TMatrixD matrix)
@@ -157,18 +157,18 @@ TF1* fit(UCNAFierzFitter &ff) {
     for (int i=0; i<nPar; i++)
         supersum_func->SetParName(i, paramNames[i]);*/
 
-	/// Actually do the combined fitting.
+    /// Actually do the combined fitting.
     global_ff = &ff; /// TODO cannot be called in a member
     TMatrixD cov(nPar,nPar);
-	ff.combined_fit(cov, asymmetry_func, /*supersum_func,*/ &combined_chi2); /// TODO no member
-	//ff.compute_fit(&asymmetry_func);
+    ff.combined_fit(cov, asymmetry_func, /*supersum_func,*/ &combined_chi2); /// TODO no member
+    //ff.compute_fit(&asymmetry_func);
     global_ff = 0;
 
     double *p = asymmetry_func->GetParameters();
     if (FIT_TYPE=="AbN") {
         assert(nPar == 3);
         double A=p[0], b=p[1], N=p[2];
-	    ff.compute_fit(A,b,N);
+        ff.compute_fit(A,b,N);
     }
     else if (FIT_TYPE=="bN") {
         assert(nPar == 2);
@@ -184,11 +184,11 @@ TF1* fit(UCNAFierzFitter &ff) {
 
     /// Look up sizes
     double Nsim_data = ff.data.super_sum.GetEntries();
-	double Nall_data = ff.data.super_sum.GetEffectiveEntries(KEmin, KEmax);
-	double Nfit_data = ff.data.super_sum.GetEffectiveEntries(fit_min, fit_max);
-	double Nfit_vector = ff.vector.super_sum.GetEffectiveEntries(fit_min, fit_max);
-	double Nfit_axial = ff.axial.super_sum.GetEffectiveEntries(fit_min, fit_max);
-	double Nfit_fierz = ff.fierz.super_sum.GetEffectiveEntries(fit_min, fit_max);
+    double Nall_data = ff.data.super_sum.GetEffectiveEntries(KEmin, KEmax);
+    double Nfit_data = ff.data.super_sum.GetEffectiveEntries(fit_min, fit_max);
+    double Nfit_vector = ff.vector.super_sum.GetEffectiveEntries(fit_min, fit_max);
+    double Nfit_axial = ff.axial.super_sum.GetEffectiveEntries(fit_min, fit_max);
+    double Nfit_fierz = ff.fierz.super_sum.GetEffectiveEntries(fit_min, fit_max);
   
     /// Set up reasonable guesses 
     double A = -0.12;
@@ -201,41 +201,41 @@ TF1* fit(UCNAFierzFitter &ff) {
 
     /// PRINT OUT REPORT OF FITS, CORRELATIONS AND ERRORS
 
-	/// Output the data and cut info.
+    /// Output the data and cut info.
     cout<<setprecision(5);
-	cout<<" ENERGY RANGE:\n";
-	cout<<"    Full energy range is "<<KEmin<<" - "<<KEmax<<" keV.\n";
-	cout<<"    Fit energy range cut is "<<fit_min<<" - "<<fit_max<<" keV.\n";
-	cout<<"    Number of actual counts in data is "<<int(Nsim_data)<<".\n";
-	cout<<"    Effective number of counts in full energy range is "<<int(Nall_data)<<".\n";
-	cout<<"    Effective number of counts in fit energy range cut is "<<int(Nfit_data)<<".\n";
-	cout<<"    Efficiency of energy cut is "<<int(Nfit_data/Nall_data*1000)/10<<"%.\n";
-	cout<<"    Effective N "<<int(N*100)/100<<".\n";
+    cout<<" ENERGY RANGE:\n";
+    cout<<"    Full energy range is "<<KEmin<<" - "<<KEmax<<" keV.\n";
+    cout<<"    Fit energy range cut is "<<fit_min<<" - "<<fit_max<<" keV.\n";
+    cout<<"    Number of actual counts in data is "<<int(Nsim_data)<<".\n";
+    cout<<"    Effective number of counts in full energy range is "<<int(Nall_data)<<".\n";
+    cout<<"    Effective number of counts in fit energy range cut is "<<int(Nfit_data)<<".\n";
+    cout<<"    Efficiency of energy cut is "<<int(Nfit_data/Nall_data*1000)/10<<"%.\n";
+    cout<<"    Effective N "<<int(N*100)/100<<".\n";
     
-	/// Set all expectation values for this range.
+    /// Set all expectation values for this range.
     const int nSpec = 4;
     TMatrixD ex(nSpec,nSpec);
     TString colNames[nSpec], rowNames[nSpec];
-	for (int m=0; m<nSpec; m++) {       /// beta exponent
+    for (int m=0; m<nSpec; m++) {       /// beta exponent
         colNames[m].Form("(v/c)^%d",m);
-		for (int n=0; n<nSpec; n++) {  /// m_e/E exponent
+        for (int n=0; n<nSpec; n++) {  /// m_e/E exponent
             rowNames[n].Form("(m/E)^%d",n); 
-			ex[m][n] = evaluate_expected_fierz(m,n,fit_min,fit_max);
+            ex[m][n] = evaluate_expected_fierz(m,n,fit_min,fit_max);
             //cout<<"Expected value for beta exponent: "<<m
             //    <<" and m/E exponent "<<n<<" is "<<ex[m][n]<<".\n";
         }
     }
 
-	output_matrix("APPROXIMATE ANALYTICAL EXPECTATION MATRIX", ex, colNames, rowNames);
-	
-	/// Calculate the predicted inverse covariance matrix for this range.
-	TMatrixD est_cov_inv(nPar,nPar);
-	for (int i=0; i<nPar; i++)
-		for (int j=0; j<nPar; j++)
-	        est_cov_inv[i][j] = 0;
+    output_matrix("APPROXIMATE ANALYTICAL EXPECTATION MATRIX", ex, colNames, rowNames);
+    
+    /// Calculate the predicted inverse covariance matrix for this range.
+    TMatrixD est_cov_inv(nPar,nPar);
+    for (int i=0; i<nPar; i++)
+        for (int j=0; j<nPar; j++)
+            est_cov_inv[i][j] = 0;
 
     if (A_index >= 0)
-	    est_cov_inv[A_index][A_index] += N*ec2*ex[2][0];
+        est_cov_inv[A_index][A_index] += N*ec2*ex[2][0];
 
     if (b_index >= 0) {
         est_cov_inv[b_index][b_index] += N*(ex[0][2] - ex[0][1]*ex[0][1]);
@@ -247,7 +247,7 @@ TF1* fit(UCNAFierzFitter &ff) {
     }
 
     if (N_index >= 0) {
-	    est_cov_inv[N_index][N_index] = N;
+        est_cov_inv[N_index][N_index] = N;
         if (A_index >= 0) {
             est_cov_inv[A_index][N_index] =
             est_cov_inv[N_index][A_index] = N*ex[0][1];
@@ -255,7 +255,7 @@ TF1* fit(UCNAFierzFitter &ff) {
     }
 
 /*  if (b_index > -1)
-	    est_cov_inv[0][0] = N*ex_cos*ex[2][0];
+        est_cov_inv[0][0] = N*ex_cos*ex[2][0];
     if (nPar > 1) {
         est_cov_inv[1][0] = 
         est_cov_inv[0][1] = -N*A*ex_cos*ex[2][1];
@@ -263,49 +263,49 @@ TF1* fit(UCNAFierzFitter &ff) {
         est_cov_inv[1][1] = N*(A*A*ex_cos*ex[2][2] + ex[0][2] - ex[0][1]*ex[0][1]);
     }
     if (nPar > 2) {
-	    est_cov_inv[1][2] =
-	    est_cov_inv[2][1] = N*ex[0][1];
-	    est_cov_inv[2][2] = N;
+        est_cov_inv[1][2] =
+        est_cov_inv[2][1] = N*ex[0][1];
+        est_cov_inv[2][2] = N;
     } */
 
-	/// Output the fit covariance matrix from the fit.
-	output_matrix("FIT COVARIANCE MATRIX", cov);
+    /// Output the fit covariance matrix from the fit.
+    output_matrix("FIT COVARIANCE MATRIX", cov);
 
-	/// Compute the estimated covariance matrix by inverse.
-	double det = 0;
-	TMatrixD est_cov = est_cov_inv;
-	output_matrix("ESTIMATED COVARIANCE MATRIX", est_cov.Invert(&det));
+    /// Compute the estimated covariance matrix by inverse.
+    double det = 0;
+    TMatrixD est_cov = est_cov_inv;
+    output_matrix("ESTIMATED COVARIANCE MATRIX", est_cov.Invert(&det));
 
-	/// Output the estimated covariance details.
+    /// Output the estimated covariance details.
     TMatrixD ratio_cov(nPar,nPar);
-	for (int i=0; i<nPar; i++)
-		for (int j=0; j<nPar; j++)
-			ratio_cov[i][j] = est_cov[i][j]/cov[i][j];
-	output_matrix("ESTIMATED/ACTUAL COVARIANCE RATIOS", ratio_cov);
+    for (int i=0; i<nPar; i++)
+        for (int j=0; j<nPar; j++)
+            ratio_cov[i][j] = est_cov[i][j]/cov[i][j];
+    output_matrix("ESTIMATED/ACTUAL COVARIANCE RATIOS", ratio_cov);
 
-	/// Output the fit covariance inverse matrix time 2.
-	TMatrixD cov_inv = cov;
-	output_matrix("FIT HALF CHI^2 DERIVATIVE MATRIX", cov_inv.Invert(&det));
+    /// Output the fit covariance inverse matrix time 2.
+    TMatrixD cov_inv = cov;
+    output_matrix("FIT HALF CHI^2 DERIVATIVE MATRIX", cov_inv.Invert(&det));
 
-	/// Compute the estimated covariance matrix by inverse.
-	output_matrix("ESTIMATED HALF CHI^2 DERIVATIVE MATRIX", est_cov_inv);
+    /// Compute the estimated covariance matrix by inverse.
+    output_matrix("ESTIMATED HALF CHI^2 DERIVATIVE MATRIX", est_cov_inv);
 
-	/// Output the estimated covariance details.
+    /// Output the estimated covariance details.
     TMatrixD ratio_cov_inv(nPar,nPar);
-	for (int i=0; i<nPar; i++)
-		for (int j=0; j<nPar; j++)
-			ratio_cov_inv[i][j] = est_cov_inv[i][j]/cov_inv[i][j];
-	output_matrix("ESTIMATED/ACTUAL HALF CHI^2 DERIVATIVE RATIOS", ratio_cov_inv);
+    for (int i=0; i<nPar; i++)
+        for (int j=0; j<nPar; j++)
+            ratio_cov_inv[i][j] = est_cov_inv[i][j]/cov_inv[i][j];
+    output_matrix("ESTIMATED/ACTUAL HALF CHI^2 DERIVATIVE RATIOS", ratio_cov_inv);
 
 #if 0
     /// Compute independent standard errors.
-	cout<<"\n FOR UNCOMBINED FITS:\n";
-	cout<<"    Expected independent statistical sigma and error:\n";
+    cout<<"\n FOR UNCOMBINED FITS:\n";
+    cout<<"    Expected independent statistical sigma and error:\n";
     cout<<"    "<<setw(1)<<" " 
                 <<setw(cl)<<"value"
                 <<setw(cl)<<"sigma" 
                 <<setw(cl+1)<<"error\n";
-	for (int i=0; i<nPar; i++) {
+    for (int i=0; i<nPar; i++) {
         TString name = paramNames[i];
         mc_dir+"SimAnalyzed_Beta_7.root",
         double value = asymmetry_func->GetParameter(i);
@@ -319,7 +319,7 @@ TF1* fit(UCNAFierzFitter &ff) {
 #endif
 
     /// Compare predicted and actual standard errors.
-	cout<<"\n FOR COMBINED FITS:\n";
+    cout<<"\n FOR COMBINED FITS:\n";
     cout<<"    Actual and estimated combined statistical sigmas and errors:\n";
     cout<<"    "<<setw(1)<<" "
                 <<setw(cl)<<"value" 
@@ -327,16 +327,16 @@ TF1* fit(UCNAFierzFitter &ff) {
                 <<setw(3*cl/2)<<"factor (act./est.)"
                 <<setw(3*cl/2)<<"error (act./est.)" 
                 <<setw(cl+1)<<"est./actual\n";
-	for (int i=0; i<nPar; i++) {
+    for (int i=0; i<nPar; i++) {
         TString param = paramNames[i];
         double scale = Sqrt(N);
         double value = asymmetry_func->GetParameter(i);
-	    double sigma = Sqrt(cov[i][i]);
-	    double error = sigma*100/value;
-	    double factor = scale*sigma;
-	    double est_sigma = Sqrt(est_cov[i][i]);
-	    double est_error = est_sigma*100/value;
-	    double est_factor = scale*est_sigma;
+        double sigma = Sqrt(cov[i][i]);
+        double error = sigma*100/value;
+        double factor = scale*sigma;
+        double est_sigma = Sqrt(est_cov[i][i]);
+        double est_error = est_sigma*100/value;
+        double est_factor = scale*est_sigma;
         double ratio = est_sigma/sigma;
         cout<<"    "<<setw(1)   <<param 
                     <<setw(cl)  <<value 
@@ -358,13 +358,13 @@ TF1* fit(UCNAFierzFitter &ff) {
                     <<setw(cl)<<"act. sq(N)"
                     <<setw(cl)<<"est. sq(N)" 
                     <<setw(cl+1)<<"est./actual\n";
-	for (int i=0; i<nPar; i++) {
+    for (int i=0; i<nPar; i++) {
         TString name_i = paramNames[i];
-	    for (int j = i+1; j<nPar; j++) {
+        for (int j = i+1; j<nPar; j++) {
             TString name_j = paramNames[j];
             TString cor_name = "cor("+name_i+","+name_j+")";
             double cor_ij = cov[j][i]/Sqrt(cov[i][i]*cov[j][j]);
-	        double est_cor_ij = est_cov[j][i]/Sqrt(est_cov[i][i]*est_cov[j][j]);
+            double est_cor_ij = est_cov[j][i]/Sqrt(est_cov[i][i]*est_cov[j][j]);
             double ratio = est_cor_ij/cor_ij;
             cout<<"    "<<setw(8)<<cor_name
                         <<setw(cl)<<cor_ij
@@ -383,8 +383,8 @@ TF1* fit(UCNAFierzFitter &ff) {
 /// MAIN APPLICATION
 int main(int argc, char *argv[])
 {
-	//TApplication app("Extract Combined A+b Fitter", &argc, argv);
-	srand( time(NULL) );    /// set this to make random or repeatable
+    //TApplication app("Extract Combined A+b Fitter", &argc, argv);
+    srand( time(NULL) );    /// set this to make random or repeatable
 
     UCNAFierzFitter ucna("monte_carlo", "Monte Carlo UCNA", KEbins, KEmin, KEmax, fit_bins, fit_min, fit_max);
     UCNAFierzFitter fake("fake", "Fake", KEbins, KEmin, KEmax, fit_bins, fit_min, fit_max);
@@ -415,23 +415,19 @@ int main(int argc, char *argv[])
         data_dir+"Range_0-1000/CorrectAsym/CorrectedAsym.root",
         "hAsym_Corrected_C",
         "2010 final official asymmetry");
-
     /// Load the files that already contain data super histogram.
     ucna.data.super_sum.fill(
         data_dir+"OctetAsym_Offic.root",
         "Total_Events_SuperSum",
         "2010 final official supersum");
-
     /// Load Monte Carlo simulated Standard Model events
     ucna.sm.fill(mc_dir+"SimAnalyzed_Beta_7.root",
                "SimAnalyzed",
                "Monte Carlo Standard Model beta spectrum");
-
     /// Load Monte Carlo simulated Fierz events
     ucna.fierz.fill(mc_dir+"SimAnalyzed_Beta_fierz_7.root",
                   "SimAnalyzed",
                   "Monte Carlo Fierz beta spectrum");
-
     //fit(ucna);
     //display(ucna);
     */
@@ -510,7 +506,6 @@ int main(int argc, char *argv[])
         mc_dir+"SimAnalyzed_Beta_9.root",
         "SimAnalyzed",
         "Monte Carlo Standard Model beta spectrum", afp_ratio, A, b);
-
     fake.data.asymmetry.fill(
         data_dir+"Range_0-1000/CorrectAsym/CorrectedAsym.root",
         "hAsym_Corrected_C",
@@ -544,7 +539,7 @@ int main(int argc, char *argv[])
     ucna.fit.super_sum.snapshot();
     ucna.display(plot_filebase);
 
-	/// extract most critical results from fit functions and report
+    /// extract most critical results from fit functions and report
     ofstream rofs;
     // time_t t = time(0);   // get time now
     // struct tm * now = localtime(&t);
@@ -561,41 +556,40 @@ int main(int argc, char *argv[])
         outfilename = plots_dir+argv[1];
     rofs.open(outfilename, std::fstream::app | std::fstream::out);
     cout << " FINAL REPORT\n";
-	cout<<setw(cl)<<"chi^2"<<setw(cl)<<"ndf"<<setw(cl)<<"chi^2/ndf"<<setw(cl)<<"p(chi2,ndf)";
-	for (int i=0; i<nPar; ++i) {  
+    cout<<setw(cl)<<"chi^2"<<setw(cl)<<"ndf"<<setw(cl)<<"chi^2/ndf"<<setw(cl)<<"p(chi2,ndf)";
+    for (int i=0; i<nPar; ++i) {  
         TString par_name = fit_func->GetParName(i);
         cout<<setw(cl)<<par_name
             <<setw(cl-5)<<par_name<<" err."
             <<setw(cl-3)<<par_name<<" ex"
             <<setw(cl-8)<<par_name<<" ex err.";
-	}
+    }
     cout<<"\n";
 
-	double chi2 = fit_func->GetChisquare(); /// Get the chi squared for this fit
-	double ndf = fit_func->GetNDF();        /// Get the degrees of freedom in fit
+    double chi2 = fit_func->GetChisquare(); /// Get the chi squared for this fit
+    double ndf = fit_func->GetNDF();        /// Get the degrees of freedom in fit
     double prob = Prob(chi2,ndf);           /// Get the probability of this chi2 with this ndf
     
-	cout<<setw(cl)<<chi2<<setw(cl)<<ndf
+    cout<<setw(cl)<<chi2<<setw(cl)<<ndf
         <<setw(cl)<<chi2/ndf<<setw(cl)<<prob;
-	rofs<<setw(cl)<<chi2<<setw(cl)<<ndf
+    rofs<<setw(cl)<<chi2<<setw(cl)<<ndf
         <<setw(cl)<<chi2/ndf<<setw(cl)<<prob;
 
-	for (int i=0; i<nPar; ++i) {  
+    for (int i=0; i<nPar; ++i) {  
         double param = fit_func->GetParameter(i);
         double error = fit_func->GetParError(i);
         cout<<setw(cl)<<param<<setw(cl)<<error
             <<setw(cl)<<param*prob<<setw(cl)<<error*prob;
         rofs<<setw(cl)<<param<<setw(cl)<<error
             <<setw(cl)<<param*prob<<setw(cl)<<error*prob;
-	}
+    }
     cout<<"\n";
     rofs<<"\n";
     rofs.close();
 
-	//app.Run();
-	return 0;
+    //app.Run();
+    return 0;
 }
-
 
 
 
