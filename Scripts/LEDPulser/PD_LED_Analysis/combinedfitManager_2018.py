30ed7385d55a4e38ba091ff2a22205b3ee9ccc38
Simon Slutsky <simonslutsky@gmail.com>
2018-09-18 14:37:27 -0700

    Changed residuals plot to % for legibility.

---
+++ Scripts/LEDPulser/PD_LED_Analysis/combinedfitManager_2018.py
@@ -0,0 +1,20 @@
+# small script to run pd_led_pmt_combinedfit.cc over a series of runs
+# Updated for 2018 
+
+import os
+import sys
+
+if __name__== "__main__":
+    startrun = int(sys.argv[1])
+    endrun = int(sys.argv[2]) + 1
+    
+    print "Commands:"
+    for run in range(startrun, endrun):
+        print "./pd_led_pmt_combinedfit_09_2018_analysis "+ str(run)
+
+    for run in range(startrun, endrun):
+        print "Doing:"
+        print "./pd_led_pmt_combinedfit_09_2018_analysis "+ str(run)
+        os.system("./pd_led_pmt_combinedfit_09_2018_analysis "+ str(run))
+
+
